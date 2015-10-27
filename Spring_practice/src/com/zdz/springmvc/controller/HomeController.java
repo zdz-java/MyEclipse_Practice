c91bd333551f7dbf0f49664f5435c6312adbecf5
zdz <zoudingzhong@foxmail.com>
2015-10-27 20:24:46 +0800

    solve the bug of context config

--- Spring_practice/src/com/zdz/springmvc/controller/HomeController.java
+++ Spring_practice/src/com/zdz/springmvc/controller/HomeController.java
@@ -4,18 +4,20 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.zdz.springmvc.service.TestService;
 @Controller
 public class HomeController {
+	
 	private TestService testService;
 	public HomeController()
 	{
 		
 	}
-//	@Inject
+	@Inject
 	public HomeController(TestService testService)
 	{
 		this.testService = testService;
