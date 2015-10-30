07f3369d60e67b6ed836d934e03b36a9c52eedc8
zdz <zoudingzhong@foxmail.com>
2015-10-30 20:43:29 +0800

    the begin of MyBatis

---
+++ MyBatis_practice/test/com/zdz/test/MyBatisTest.java
@@ -0,0 +1,42 @@
+package com.zdz.test;
+
+import java.io.Reader;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.zdz.mybatis.model.User;
+
+public class MyBatisTest {
+	private static SqlSessionFactory sqlSessionFactory;
+	private static Reader reader;
+	static {
+		try {
+			reader = Resources.getResourceAsReader("Configuration.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SqlSessionFactory getSession() {
+		return sqlSessionFactory;
+	}
+	@Test
+	public void firstTest() {
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+			User user = (User) session.selectOne(
+					"com.yihaomen.mybatis.models.UserMapper.selectUserByID", 1);
+			System.out.println(user.getUserAddress());
+			System.out.println(user.getUserName());
+		} finally {
+			session.close();
+		}
+	}
+}
