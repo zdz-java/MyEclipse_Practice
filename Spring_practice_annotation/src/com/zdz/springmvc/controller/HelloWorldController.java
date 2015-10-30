bfcf7dc696415647b00b2e046f75db36712822c9
zdz <zoudingzhong@foxmail.com>
2015-10-30 19:44:33 +0800

    first time to annotation

--- Spring_practice_annotation/src/com/zdz/springmvc/controller/HelloWorldController.java
+++ Spring_practice_annotation/src/com/zdz/springmvc/controller/HelloWorldController.java
@@ -1,17 +1,42 @@
 package com.zdz.springmvc.controller;
 
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
+//@RequestMapping(value="/zdz")
 public class HelloWorldController {
-	@RequestMapping(value="/hello")
+	@RequestMapping(value={"/hello/**","/home/{userid}"})
 	public ModelAndView helloWorld()
 	{
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("hello");
 		mv.addObject("message", "Hello World");
 		return mv;
+	}
+	@RequestMapping(value="method",method=RequestMethod.GET)
+	public String get()
+	{
+		System.out.println("get");
+		return "hello";
+	}
+	@RequestMapping(value="method",method=RequestMethod.POST)
+	public String post()
+	{
+		System.out.println("post");
+		return "hello";
+	}
+	@RequestMapping(params="a")
+	public String params()
+	{
+		System.out.println("params");
+		return "hello";
+	}
+	@RequestMapping(params="b=a")
+	public String params2()
+	{
+		System.out.println("params2");
+		return "hello";
 	}
 }
