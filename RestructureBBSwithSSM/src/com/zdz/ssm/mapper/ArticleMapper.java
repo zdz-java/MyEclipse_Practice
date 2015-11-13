002e4768daede546cba856451046ddec520729ba
zdz <zoudingzhong@foxmail.com>
2015-11-13 22:15:36 +0800

    complete part of task in dao level

--- RestructureBBSwithSSM/src/com/zdz/ssm/mapper/ArticleMapper.java
+++ RestructureBBSwithSSM/src/com/zdz/ssm/mapper/ArticleMapper.java
@@ -4,4 +4,6 @@ import com.zdz.ssm.model.Article;
 
 public interface ArticleMapper {
 	public Article getArticleById(int id);
+	public void save(Article article);
+	public void deleteById(int id);
 }	
--- RestructureBBSwithSSM/test/com/zdz/ssm/mapper/ArticleMapperTest.java
+++ RestructureBBSwithSSM/test/com/zdz/ssm/mapper/ArticleMapperTest.java
@@ -2,26 +2,50 @@ package com.zdz.ssm.mapper;
 
 import java.io.IOException;
 import java.io.InputStream;
-
+import java.util.Date;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.zdz.ssm.model.Article;
 
 public class ArticleMapperTest {
-	@Test
-	public void getArticleByIdTest() throws IOException {
+	SqlSessionFactory sqlSessionFactory;
+
+	@Before
+	public void prepare() throws IOException {
 		String resource = "Configuration.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
-				.build(inputStream);
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+
+	@Test
+	public void getArticleByIdTest() throws IOException {
 		SqlSession ss = sqlSessionFactory.openSession();
 		ArticleMapper articleMapper = ss.getMapper(ArticleMapper.class);
+
 		Article article = articleMapper.getArticleById(11);
-		Assert.assertEquals(article.getTitle(),"一个练习用的小小论坛，欢迎留言");
+		Assert.assertEquals(article.getTitle(), "一个练习用的小小论坛，欢迎留言");
+		Assert.assertEquals(article.getIsLeaf(), false);
 	}
+
+	@Test
+	public void saveAndDeleteTest() {
+		SqlSession ss = sqlSessionFactory.openSession();
+		ArticleMapper articleMapper = ss.getMapper(ArticleMapper.class);
+
+		Article article = new Article();
+		article.setTitle("23333");
+		article.setPdate(new Date());
+		articleMapper.save(article);
+		ss.commit();
+		
+		articleMapper.deleteById(article.getId());
+		ss.commit();
+	}
+	
 }
