a393efefbdafc04433bd77a1a5243f8d6a7537bc
zdz <zoudingzhong@foxmail.com>
2015-11-21 21:05:25 +0800

    finish a total mapper

--- CRM/src/main/java/com/zdz/crm/mapper/SysUserMapper.java
+++ CRM/src/main/java/com/zdz/crm/mapper/SysUserMapper.java
@@ -7,6 +7,7 @@ import com.zdz.crm.model.SysUser;
 import com.zdz.crm.util.PageResult;
 
 public interface SysUserMapper {
+//	这个方法的业务逻辑不了解，留待
 	public abstract PageResult findAll(Map paramMap);
 	
 	public abstract void save(SysUser transientInstance);
@@ -14,7 +15,7 @@ public interface SysUserMapper {
 	public abstract void delete(SysUser persistentInstance);
 
 	public abstract SysUser findById(java.lang.Long id);
-
+//	这个方法的业务逻辑不了解，留待
 	public abstract List findByExample(SysUser instance);
 
 	public abstract List findByProperty(String propertyName, Object value);
@@ -24,10 +25,10 @@ public interface SysUserMapper {
 	public abstract List findByUsrPassword(Object usrPassword);
 
 	public abstract List findByUsrFlag(Object usrFlag);
-
+//	这个方法的业务逻辑不了解，留待
 	public abstract SysUser merge(SysUser detachedInstance);
-
+//	这个方法的业务逻辑不了解，留待
 	public abstract void attachDirty(SysUser instance);
-
+//	这个方法的业务逻辑不了解，留待
 	public abstract void attachClean(SysUser instance);
 }
--- CRM/src/test/java/com/zdz/crm/mapper/SysUserMapperTest.java
+++ CRM/src/test/java/com/zdz/crm/mapper/SysUserMapperTest.java
@@ -1,8 +1,11 @@
 package com.zdz.crm.mapper;
 
 import static org.junit.Assert.*;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,24 +13,111 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.zdz.crm.model.SysRole;
 import com.zdz.crm.model.SysUser;
 
 public class SysUserMapperTest {
+	private String resource;
+	private InputStream inputStream;
+	private SqlSessionFactory sqlSessionFactory;
 	@Before
-	public void prepare()
-	{
-		
+	public void prepare() throws IOException {
+		resource = "mybatis-config.xml";
+		inputStream = Resources.getResourceAsStream(resource);
+		sqlSessionFactory = new SqlSessionFactoryBuilder()
+				.build(inputStream);
 	}
+
 	@Test
-	public void findByIdTest() throws IOException
-	{
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	public void findByIdTest() throws IOException {
 		SqlSession session = sqlSessionFactory.openSession();
 		SysUserMapper sysUserMapper = session.getMapper(SysUserMapper.class);
-		SysUser user = sysUserMapper.findById((long) 2);
-		assertEquals(user.getUsrName(),"admin");
+		
+		SysUser sysuser = sysUserMapper.findById((long) 2);
+		assertEquals(sysuser.getUsrName(), "admin");
+		assertEquals(sysuser.getSysRole().getRoleName(), "系统管理员");
+		session.commit();
+		session.close();
+	}
+	
+	@Test
+	public void saveAndDeleteTest() throws IOException {
+		SqlSession session = sqlSessionFactory.openSession();
+		SysUserMapper sysUserMapper = session.getMapper(SysUserMapper.class);
+		
+		SysUser user = new SysUser();
+		SysRole role = new SysRole();
+		role.setRoleId((long)6);
+		
+		user.setUsrFlag(1);
+		user.setUsrName("zdz");
+		user.setUsrPassword("121");
+		user.setSysRole(role);
+		
+		sysUserMapper.save(user);
+		session.commit();
+		
+		SysUser getUser = sysUserMapper.findById(user.getUsrId());
+		assertEquals(getUser.getUsrName(),"zdz");
+		assertEquals((long)getUser.getSysRole().getRoleId(),6);
+		
+		sysUserMapper.delete(user);
+		session.commit();
+		session.close();
+	}
+	
+	@Test
+	public void findByTest() throws IOException {
+		SqlSession session = sqlSessionFactory.openSession();
+		SysUserMapper sysUserMapper = session.getMapper(SysUserMapper.class);
+		
+		SysUser user = new SysUser();
+		
+		user.setUsrFlag(1);
+		user.setUsrName("zdz");
+		user.setUsrPassword("121");
+		
+		sysUserMapper.save(user);
+		session.commit();
+		
+		List<SysUser> users;
+		users = sysUserMapper.findByUsrName("zdz");
+		assertEquals(users.get(users.size()-1).getUsrPassword(),"121");
+		users = sysUserMapper.findByUsrPassword("121");
+		assertEquals(users.get(users.size()-1).getUsrName(),"zdz");
+		users = sysUserMapper.findByUsrFlag(1);
+		assertEquals(users.get(users.size()-1).getUsrName(),"zdz");
+		
+		sysUserMapper.delete(user);
+		session.commit();
+		session.close();
+	}
+	
+	@Test
+	public void findByPropertyTest() throws IOException {
+		SqlSession session = sqlSessionFactory.openSession();
+		SysUserMapper sysUserMapper = session.getMapper(SysUserMapper.class);
+		
+		SysUser user = new SysUser();
+		
+		user.setUsrFlag(1);
+		user.setUsrName("zdz");
+		user.setUsrPassword("121");
+		
+		sysUserMapper.save(user);
+		session.commit();
+		
+		List<SysUser> users;
+		users = sysUserMapper.findByProperty("usrName","zdz");
+		assertEquals(users.get(users.size()-1).getUsrPassword(),"121");
+		users = sysUserMapper.findByProperty("usrPassword","121");
+		assertEquals(users.get(users.size()-1).getUsrName(),"zdz");
+		users = sysUserMapper.findByProperty("usrFlag",1);
+		assertEquals(users.get(users.size()-1).getUsrName(),"zdz");
+		users = sysUserMapper.findByProperty("not","not");
+		assertEquals(users.size(),0);
+		
+		sysUserMapper.delete(user);
 		session.commit();
 		session.close();
 	}
