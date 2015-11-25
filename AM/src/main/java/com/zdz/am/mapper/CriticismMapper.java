62e882f18e398c7bb81387759ff45774046cca9b
zdz <zoudingzhong@foxmail.com>
2015-11-25 23:24:59 +0800

    finish criticismmapper and the test

--- AM/src/main/java/com/zdz/am/mapper/CriticismMapper.java
+++ AM/src/main/java/com/zdz/am/mapper/CriticismMapper.java
@@ -5,5 +5,6 @@ import com.zdz.am.model.Criticism;
 public interface CriticismMapper {
 	public void addCriticism(Criticism criticism);			//�����
 	public Criticism findCriticismByMsgID(int messageID);	//������ID������
+	public void deleteCriticismByMsgID(int messageID);	//������ID������
 
 }
--- AM/src/test/java/com/zdz/am/mapper/CriticismMapperTest.java
+++ AM/src/test/java/com/zdz/am/mapper/CriticismMapperTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -28,13 +27,33 @@ public class CriticismMapperTest {
 				.build(inputStream);
 	}
 	@Test
-	public void findByIdTest() throws IOException {
+	public void findCriticismByMsgIDTest() throws IOException {
 		SqlSession session = sqlSessionFactory.openSession();
 		CriticismMapper criticismMapper = session.getMapper(CriticismMapper.class);
 		
 		Criticism criticism = criticismMapper.findCriticismByMsgID(3);
 		assertEquals(criticism.getEmployeeID(), 3052);
 		assertEquals(criticism.getCriticismContent(), "<p>不错，以后大家好好干活！！</p>");
+		session.commit();
+		session.close();
+	}
+	@Test
+	public void addCriticismTest()
+	{
+		SqlSession session = sqlSessionFactory.openSession();
+		CriticismMapper criticismMapper = session.getMapper(CriticismMapper.class);
+		
+		Criticism toinsert = new Criticism();
+		toinsert.setCriticismContent("temp");
+		toinsert.setMessageID(16);
+		toinsert.setEmployeeID(3052);
+		criticismMapper.addCriticism(toinsert);
+		session.commit();
+		
+		Criticism criticism = criticismMapper.findCriticismByMsgID(toinsert.getCriticismID());
+		assertEquals(criticism.getMessageID(), 16);
+		assertEquals(criticism.getCriticismContent(), "temp");
+		criticismMapper.deleteCriticismByMsgID(toinsert.getCriticismID());
 		session.commit();
 		session.close();
 	}
