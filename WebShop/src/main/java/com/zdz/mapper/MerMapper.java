7c3bb0098e04e8789763d2270bfb92775d2443e7
zdz <zoudingzhong@foxmail.com>
2015-12-23 20:49:41 +0800

    add method realize

---
+++ WebShop/src/main/java/com/zdz/mapper/MerMapper.java
@@ -0,0 +1,26 @@
+package com.zdz.mapper;
+
+import java.util.List;
+
+import com.zdz.model.Category;
+import com.zdz.model.Merchandise;
+
+public interface MerMapper {
+//	还没有搞懂hql具体指的是什么，猜测为标题的搜索内容，两个关于分页的暂留
+	/** ��ҳ�����Ʒ */
+	public List browseMer(int pageSize,int pageNo,int cateId,boolean isSpecial);
+	/** ������Ʒ */
+	public List browseMer(int pageSize,int pageNo,String hql);	
+	
+	public List browseCategory();	
+	public Category loadCategory(Integer id);	
+	public boolean delCategory(Integer id);	
+	public boolean addCategory(Category cate);	
+	public boolean updateCategory(Category cate);
+	public List browseMer(String hql);	
+	public Merchandise loadMer(Integer id);	
+	public boolean delMer(Integer id);	
+	public boolean addMer(Merchandise mer);	
+	public boolean updateMer(Merchandise mer);
+	public int countRecord(String hql);	
+}
