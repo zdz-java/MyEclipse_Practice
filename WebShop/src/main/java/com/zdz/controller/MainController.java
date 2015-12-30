67d6f8d45adefd488f17d3af86b08da80ac63825
zdz <zoudingzhong@foxmail.com>
2015-12-30 22:23:15 +0800

    build the basic SpringMVC framework

---
+++ WebShop/src/main/java/com/zdz/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.zdz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/test")
+	public String testController()
+	{
+		return "test";
+	}
+}
