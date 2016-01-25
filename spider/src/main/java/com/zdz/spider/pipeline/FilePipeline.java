69b816582790bec2b4847ec28dbbc7a5cf63b1da
zdz <zoudingzhong@foxmail.com>
2016-01-25 20:43:39 +0800

    finish pipeline test

--- spider/src/main/java/com/zdz/spider/pipeline/FilePipeline.java
+++ spider/src/main/java/com/zdz/spider/pipeline/FilePipeline.java
@@ -1,5 +1,12 @@
 package com.zdz.spider.pipeline;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import com.zdz.spider.util.ResultItem;
 
 public class FilePipeline {
@@ -14,6 +21,21 @@ public class FilePipeline {
 	}
 
 	public void process(ResultItem resultItem) {
-		
+		if(resultItem == null)
+		{
+			throw new NullPointerException("null resultItem");
+		}
+		File file = new File(path+"/"+resultItem.getUrl()+".txt");
+		try {
+			file.createNewFile();
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+			bufferedWriter.write(resultItem.getHtml());
+//			fileOutputStream.close();
+			bufferedWriter.flush();
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- spider/src/test/java/com/zdz/spider/MainTest.java
+++ spider/src/test/java/com/zdz/spider/MainTest.java
@@ -21,25 +21,25 @@ import com.zdz.spider.util.Request;
 import com.zdz.spider.util.ResultItem;
 
 public class MainTest {
-	@Test
-	public void downloaderTest()
-	{
-		Downloader d = new Downloader();
-		Request r = new Request();
-		
-		String url = "http://www.baidu.com/";
-		r.setUrl(url);
-		
-		Scheduler s = new Scheduler();
-		s.put(r);
-		Request r2 = s.take();
-		
-		
-		Page p = d.download(r2);
-
-		Assert.assertEquals(p.getUrl(), url);
-		Assert.assertEquals(p.getStatusCode(), 200);
-	}
+//	@Test
+//	public void downloaderTest()
+//	{
+//		Downloader d = new Downloader();
+//		Request r = new Request();
+//		
+//		String url = "http://www.baidu.com/";
+//		r.setUrl(url);
+//		
+//		Scheduler s = new Scheduler();
+//		s.put(r);
+//		Request r2 = s.take();
+//		
+//		
+//		Page p = d.download(r2);
+//
+//		Assert.assertEquals(p.getUrl(), url);
+//		Assert.assertEquals(p.getStatusCode(), 200);
+//	}
 	@Test
 	public void pageProcesserTest()
 	{
@@ -57,15 +57,15 @@ public class MainTest {
 		Assert.assertEquals(s.take().getUrl(), url);
 		
 	}
-	
+	@Test
 	public void pipelineTest()
 	{
 		FilePipeline p = new FilePipeline();
-		String path = "";
+		String path = "c:/_forjavatest";
 		p.setPath(path);
 		ResultItem resultItem = new ResultItem();
-		String url = "";
-		String html = "";
+		String url = "www.zdz.com";
+		String html = "this is the result html";
 		resultItem.setUrl(url);
 		resultItem.setHtml(html);
 		p.process(resultItem);
