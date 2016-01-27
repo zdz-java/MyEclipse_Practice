fb243422a06a4c8b8c1adf4f4d240a44d436c164
zdz <zoudingzhong@foxmail.com>
2016-01-27 10:48:53 +0800

    adjust spider

--- spider/src/main/java/com/zdz/spider/util/Spider.java
+++ spider/src/main/java/com/zdz/spider/util/Spider.java
@@ -13,10 +13,47 @@ import com.zdz.spider.scheduler.Scheduler;
 
 public class Spider implements Callable<Boolean>{
 	private BlockingQueue<String> urls = new LinkedBlockingQueue<String>(10);
-	private Downloader downloader = new Downloader();
-	private PageProcesser pageProcesser = new PageProcesser();
-	private FilePipeline filePipeline = new FilePipeline();
-	private Scheduler scheduler = new Scheduler();
+	private Downloader downloader;
+	private PageProcesser pageProcesser;
+	private FilePipeline filePipeline;
+	private Scheduler scheduler;
+	private ExecutorService executorService = Executors.newCachedThreadPool();
+	
+	public void setDownloader(Downloader downloader) {
+		this.downloader = downloader;
+	}
+	public void setPageProcesser(PageProcesser pageProcesser) {
+		this.pageProcesser = pageProcesser;
+	}
+	public void setFilePipeline(FilePipeline filePipeline) {
+		this.filePipeline = filePipeline;
+	}
+	public void setScheduler(Scheduler scheduler) {
+		this.scheduler = scheduler;
+	}
+	public void setExecutorService(ExecutorService executorService) {
+		this.executorService = executorService;
+	}
+	public Spider downloader(Downloader downloader)
+	{
+		setDownloader(downloader);
+		return this;
+	}
+	public Spider scheduler(Scheduler scheduler)
+	{
+		setScheduler(scheduler);
+		return this;
+	}
+	public Spider pageProcesser(PageProcesser pageProcesser)
+	{
+		setPageProcesser(pageProcesser);
+		return this;
+	}
+	public Spider pipeline(FilePipeline filePipeline)
+	{
+		setFilePipeline(filePipeline);
+		return this;
+	}
 	public Boolean call() throws Exception {
 		while(true)
 		{
@@ -24,22 +61,29 @@ public class Spider implements Callable<Boolean>{
 			{
 				break;
 			}
-			Request request = scheduler.take();
-			Page page = downloader.download(request);
-			ResultItem resultItem = pageProcesser.process(page);
-			filePipeline.process(resultItem);	
+			executorService.submit(new Runnable() {
+				public void run() {
+					Request request = scheduler.take();
+					Page page = downloader.download(request);
+					ResultItem resultItem = pageProcesser.process(page);
+					filePipeline.process(resultItem);	
+				}
+			});
 		}
 		
 		return null;
 	}
 	public void start()
 	{
-		ExecutorService executorService = Executors.newCachedThreadPool();
 		for(String url:urls)
 		{
 			scheduler.put(new Request(url));
 		}
-		executorService.submit(this);
+		try {
+			call();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	public void putUrl(String url)
 	{
--- spider/src/main/java/com/zdz/spider/util/UrlUtils.java
+++ spider/src/main/java/com/zdz/spider/util/UrlUtils.java
@@ -3,7 +3,6 @@ package com.zdz.spider.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//import org.apache.commons.lang3.StringUtils;
 
 public class UrlUtils {
 
@@ -12,26 +11,26 @@ public class UrlUtils {
 
 //	该方法为拷贝webmagic项目的
 	public static String fixAllRelativeHrefs(String content, String url) {
-//		content = replaceByPattern(content, url, patternForHrefWithQuote);
-//		content = replaceByPattern(content, url, patternForHrefWithoutQuote);
+		content = replaceByPattern(content, url, patternForHrefWithQuote);
+		content = replaceByPattern(content, url, patternForHrefWithoutQuote);
         return content;
 	}
-//	public static String replaceByPattern(String html, String url, Pattern pattern) {
-//        StringBuilder stringBuilder = new StringBuilder();
-//        Matcher matcher = pattern.matcher(html);
-//        int lastEnd = 0;
-//        boolean modified = false;
-//        while (matcher.find()) {
-//            modified = true;
-//            stringBuilder.append(StringUtils.substring(html, lastEnd, matcher.start()));
-//            stringBuilder.append(matcher.group(1));
-////            stringBuilder.append("\"").append(canonicalizeUrl(matcher.group(2), url)).append("\"");
-//            lastEnd = matcher.end();
-//        }
-//        if (!modified) {
-//            return html;
-//        }
-//        stringBuilder.append(StringUtils.substring(html, lastEnd));
-//        return stringBuilder.toString();
-//    }
+	public static String replaceByPattern(String html, String url, Pattern pattern) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Matcher matcher = pattern.matcher(html);
+        int lastEnd = 0;
+        boolean modified = false;
+        while (matcher.find()) {
+            modified = true;
+            stringBuilder.append(StringUtils.substring(html, lastEnd, matcher.start()));
+            stringBuilder.append(matcher.group(1));
+//            stringBuilder.append("\"").append(canonicalizeUrl(matcher.group(2), url)).append("\"");
+            lastEnd = matcher.end();
+        }
+        if (!modified) {
+            return html;
+        }
+        stringBuilder.append(StringUtils.substring(html, lastEnd));
+        return stringBuilder.toString();
+    }
 }
