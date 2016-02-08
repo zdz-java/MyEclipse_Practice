50e35e2bbf8c131c1f613ab0df4dc0998d161201
zdz <zoudingzhong@foxmail.com>
2016-02-08 18:33:09 +0800

    adjust

--- WebShop/src/main/java/com/zdz/controller/MainController.java
+++ WebShop/src/main/java/com/zdz/controller/MainController.java
@@ -19,6 +19,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 import com.zdz.model.Admin;
 import com.zdz.model.Cart;
 import com.zdz.model.Cartselectedmer;
--- WebShop/src/main/webapp/JS/order.js
+++ WebShop/src/main/webapp/JS/order.js
@@ -4,5 +4,7 @@ $(function(){
 function deleteOrder(oid)
 {
 	url = "delOrder?oid="+oid;
-	
+	$(this).get(url,function(){
+		alert("删除成功");
+	});
 };
