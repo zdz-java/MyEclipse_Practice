137db8f21092204c2a2d7ddcbe4ba4f4d531790b
zdz <zoudingzhong@foxmail.com>
2016-02-15 21:46:11 +0800

    adjust

--- LDM/src/main/java/com/zdz/model/Student.java
+++ LDM/src/main/java/com/zdz/model/Student.java
@@ -1,5 +1,19 @@
 package com.zdz.model;
 
 public class Student {
-
+	private String name;
+	private String score;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
 }
