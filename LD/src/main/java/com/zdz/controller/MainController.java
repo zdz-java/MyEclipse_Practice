687533303ab072ad65832affcaaac5411d78bfbe
zdz <zoudingzhong@foxmail.com>
2016-02-20 23:07:53 +0800

    add half spring

--- LD/src/main/java/com/zdz/controller/MainController.java
+++ LD/src/main/java/com/zdz/controller/MainController.java
@@ -3,8 +3,16 @@ package com.zdz.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.zdz.service.StudentService;
+
 @Controller
 public class MainController {
+	private StudentService studentService;
+	
+	public void setStudentService(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
 	@RequestMapping("/test")
 	public void test()
 	{
